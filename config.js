e2e70fef18418067800910c6eb9396082b2ae4ef
Piko86 <manmithabackup@gmail.com>
2025-07-15 10:28:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "OQ4xDAyB#cwefJ-TCRJB64Q4SEu-aXn4gFzpuLKwazX_JNdYyIaM",
+  SESSION_ID: process.env.SESSION_ID || "nc9VVaxJ#yWXW1-YB_LSj7Xgrx6bZJCkEhErob5qGWZ0o0XhGGxQ",
   OWNER_NUM: process.env.OWNER_NUM || "94726939427",
   OWNER_JID: process.env.OWNER_JID || "75227023323273@lid",
   PREFIX: process.env.PREFIX || ".",
