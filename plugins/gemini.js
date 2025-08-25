117ba1c0e3ac5fd96db43a2f49e5af8f3f5fbbc5
Piko86 <manmithabackup@gmail.com>
2025-08-25 18:21:43 +0530

    Update gemini.js

--- plugins/gemini.js
+++ plugins/gemini.js
@@ -3,7 +3,7 @@ const axios = require("axios")
 const config = require('../config');
 
 const GEMINI_API_KEY = config.GEMINI_API_KEY;  //REPLACE WITH YOUR API KEY OF GEMINI
-const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
+const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`;
 
 cmd({
   pattern: "gemini",
